e955b4048564a08d57747115aac18fad43594ab6
Andriy Kondrachuk <andriy.kondrachuk7@gmail.com>
2022-04-09 11:42:20 +0300

    a

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const { default: mongoose } = require("mongoose");
 
-var dburl = 'mongo'
+var dburl = 'mongodb+srv://site:sitepass@sitedb.o5gq3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
 mongoose.connect(dburl);
 mongoose.connection.on('connected',()=>{console.log('connected to ' + dburl)});
