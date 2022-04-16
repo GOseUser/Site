03d137fcba9b548bcdc7efaf6b636fd9d6824f6a
Andriy Kondrachuk <andriy.kondrachuk7@gmail.com>
2022-04-16 11:25:30 +0300

    commit

--- routes/index.js
+++ routes/index.js
@@ -3,37 +3,45 @@ var controllers = require('../controllers/indexControllers');
 var router = express.Router();
 const Note = require('../models/note');
 const { ObjectId } = require('mongodb');
+const passport = require('passport');
+const jwtd = require('jwt-decode');
 
 /* GET home page. */
 router.get('/', controllers.mainPage);
-router.get('/note', function(req, res){
-    res.render('note');
+router.get('/note', passport.authenticate('jwt', { session: false }), function (req, res) {
+    res.render('note', { token: req.query.token });
 });
-router.post('/note', async function(req,res){
+
+router.post('/note', passport.authenticate('jwt', { session: false }), async function (req, res) {
     let date = new Date();
-    let note = new Note({data:req.body.newNote, time:date.toISOString()});
+    let note = new Note({ data: req.body.newNote, time: date.toISOString(), author: jwtd(req.query.token).user.username });
     await note.save();
-    res.redirect('notes');
+    res.redirect('notes?token=' + req.query.token);
 });
 
-router.get('/notes', async function(req, res){
-    const notes = await Note.find();
-    res.render('notes',{notes});
+router.get('/notes',passport.authenticate('jwt', { session: false }), async function (req, res) {
+    let user = jwtd(req.query.token).user.username;
+    const notes = await Note.find({author: user});
+    console.log(req.query.token);
+    res.render('notes', { notes, token: req.query.token });
 });
 
-router.get('/notes/delete/:nid', async function(req, res){
-    await Note.deleteOne({_id:req.params.nid});
-    res.redirect('/notes');
+router.get('/notes/delete/:nid', passport.authenticate('jwt', { session: false }), async function (req, res) {
+    await Note.deleteOne({ _id: req.params.nid });
+    res.redirect(url.format({
+        pathname:"/notes",
+        query:req.query,
+    }));
 });
 
-router.get('/notes/edit/:nid', async function(req, res){
+router.get('/notes/edit/:nid', async function (req, res) {
     const note = await Note.findById(req.params.nid)
-    res.render('onenote',{note})
+    res.render('onenote', { note })
 });
 
-router.post('/notes/edit/:nid', async function (req, res){
+router.post('/notes/edit/:nid', async function (req, res) {
     console.log(req.params.nid);
-    await Note.findByIdAndUpdate(ObjectId(req.params.nid),{data:req.body.data});
+    await Note.findByIdAndUpdate(ObjectId(req.params.nid), { data: req.body.data });
     res.redirect('/notes');
 });
 
--- routes/users.js
+++ routes/users.js
@@ -3,20 +3,21 @@ var router = express.Router();
 const User = require("../models/user");
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
-router.get('/', function(req, res, next) {
+
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
-router.get('/register', function(req, res, next) {
+
+router.get('/register', function (req, res, next) {
   res.render('registration');
 });
 
-
-router.post('/register', passport.authenticate('signup',{session:false}),function (req, res){
+router.post('/register', passport.authenticate('signup', { session: false }), function (req, res) {
   res.send(req.user);
 });
 
-router.get('/login', (req, res)=>{
-res.render('login');
+router.get('/login', (req, res) => {
+  res.render('login');
 });
 
 router.post('/login', async (req, res, next) => {
@@ -28,19 +29,19 @@ router.post('/login', async (req, res, next) => {
           return next(error);
         }
         req.login(user, { session: false }, async (error) => {
-            if (error) return next(error);
-            const body = { _id: user._id, username: user.username };
-            const token = jwt.sign({ user: body }, 'NotesLoginSecret');
-            return res.json({ token });
-          }
+          if (error) return next(error);
+          const body = { _id: user._id, username: user.username };
+          const token = jwt.sign({ user: body }, 'NotesLoginSecret');
+          return res.render('note', { token });
+        }
         );
       } catch (error) {
         return next(error);
       }
-    }) (req, res, next);
-}
-);
-router.get('/profile', passport.authenticate('jwt', {session:false}), (req,res) =>{
-res.render('profile');
+    })(req, res, next);
+});
+
+router.get('/profile', passport.authenticate('jwt', { session: false }), (req, res) => {
+  res.render('profile', { token: req.query.token });
 });
 module.exports = router;
